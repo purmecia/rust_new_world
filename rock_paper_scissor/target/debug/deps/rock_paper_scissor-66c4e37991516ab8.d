97c504837afcb4c46eaa2a13a8d394be665dada0
Purmecia <43378915+purmecia@users.noreply.github.com>
2023-02-08 18:32:49 +0000

    new project paper

---
+++ rock_paper_scissor/target/debug/deps/rock_paper_scissor-66c4e37991516ab8.d
@@ -0,0 +1,5 @@
+/workspaces/rust_new_world/rock_paper_scissor/target/debug/deps/rock_paper_scissor-66c4e37991516ab8: src/main.rs
+
+/workspaces/rust_new_world/rock_paper_scissor/target/debug/deps/rock_paper_scissor-66c4e37991516ab8.d: src/main.rs
+
+src/main.rs:
---
+++ rock_paper_scissor/target/debug/rock_paper_scissor.d
@@ -0,0 +1,1 @@
+/workspaces/rust_new_world/rock_paper_scissor/target/debug/rock_paper_scissor: /workspaces/rust_new_world/rock_paper_scissor/src/main.rs
