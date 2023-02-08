6c088c7ef2a91c1c30e662f34379f682b7d81521
Purmecia <43378915+purmecia@users.noreply.github.com>
2023-02-08 18:56:56 +0000

    finish rock paper scissor

---
+++ rock_paper_scissor/target/debug/deps/ppv_lite86-92ecb90ab7fb5a38.d
@@ -0,0 +1,11 @@
+/workspaces/rust_new_world/rock_paper_scissor/target/debug/deps/ppv_lite86-92ecb90ab7fb5a38.rmeta: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/x86_64/mod.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/x86_64/sse2.rs
+
+/workspaces/rust_new_world/rock_paper_scissor/target/debug/deps/libppv_lite86-92ecb90ab7fb5a38.rlib: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/x86_64/mod.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/x86_64/sse2.rs
+
+/workspaces/rust_new_world/rock_paper_scissor/target/debug/deps/ppv_lite86-92ecb90ab7fb5a38.d: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/x86_64/mod.rs /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/x86_64/sse2.rs
+
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/lib.rs:
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/soft.rs:
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/types.rs:
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/x86_64/mod.rs:
+/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/ppv-lite86-0.2.17/src/x86_64/sse2.rs:
---
+++ rock_paper_scissor/target/debug/deps/rock_paper_scissor-b05dacb6ee7f28d2.d
@@ -0,0 +1,5 @@
+/workspaces/rust_new_world/rock_paper_scissor/target/debug/deps/rock_paper_scissor-b05dacb6ee7f28d2: src/main.rs
+
+/workspaces/rust_new_world/rock_paper_scissor/target/debug/deps/rock_paper_scissor-b05dacb6ee7f28d2.d: src/main.rs
+
+src/main.rs:
